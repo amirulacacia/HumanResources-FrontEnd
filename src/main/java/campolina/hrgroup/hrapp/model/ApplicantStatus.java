243ae10706eb151c90f5a4ea386702738223acfe
amirulacacia <amirulacacia@outlook.com>
2024-03-02 13:57:24 +0800

    Adding Applicant Status

--- src/main/java/campolina/hrgroup/hrapp/model/ApplicantStatus.java
+++ src/main/java/campolina/hrgroup/hrapp/model/ApplicantStatus.java
@@ -1,5 +1,59 @@
 package campolina.hrgroup.hrapp.model;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "applicant_status")
 public class ApplicantStatus {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "applicantStatus_id")
+    private Long applicantStatusId;
+
+    @Column(nullable = false, updatable = false, name = "application_date")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date applicationDate;
+
+    @NotBlank
+    private String status;
+
+    public Long getApplicantStatusId() {
+        return applicantStatusId;
+    }
+
+    public Date getApplicationDate() {
+        return applicationDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setApplicantStatusId(Long applicantStatusId) {
+        this.applicantStatusId = applicantStatusId;
+    }
+
+    public void setApplicationDate(Date applicationDate) {
+        this.applicationDate = applicationDate;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
