2dd10ce096681de90f3e71e525cc2e933c451cfe
nkhairina <nasreenkhairina@gmail.com>
2024-03-02 14:55:13 +0800

    02032024-2

--- src/main/java/campolina/hrgroup/hrapp/model/ApplicantStatus.java
+++ src/main/java/campolina/hrgroup/hrapp/model/ApplicantStatus.java
@@ -1,5 +1,12 @@
 package campolina.hrgroup.hrapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Applicant_Status")
+
+
 public class ApplicantStatus {
 
 }
