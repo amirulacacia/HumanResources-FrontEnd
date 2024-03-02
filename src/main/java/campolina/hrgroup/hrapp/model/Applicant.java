d124c437c8f26a46facc9176420e751ec2b9ccfd
nkhairina <nasreenkhairina@gmail.com>
2024-03-02 14:41:20 +0800

    02032024-2

--- src/main/java/campolina/hrgroup/hrapp/model/Applicant.java
+++ src/main/java/campolina/hrgroup/hrapp/model/Applicant.java
@@ -1,5 +1,72 @@
 package campolina.hrgroup.hrapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "applicant")
 public class Applicant {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long applicantId;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @OneToOne
+    private JobPosting jobPosting;
+
+    @OneToOne
+    private ApplicantStatus applicantStatus;
+
+    public long getApplicantId() {
+        return applicantId;
+    }
+
+    public void setApplicantId(long applicantId) {
+        this.applicantId = applicantId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public JobPosting getJobPosting() {
+        return jobPosting;
+    }
+
+    public void setJobPosting(JobPosting jobPosting) {
+        this.jobPosting = jobPosting;
+    }
+
+    public ApplicantStatus getApplicantStatus() {
+        return applicantStatus;
+    }
+
+    public void setApplicantStatus(ApplicantStatus applicantStatus) {
+        this.applicantStatus = applicantStatus;
+    }
 }
